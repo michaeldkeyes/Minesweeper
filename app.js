9652cee6e37e5c058add51745be36dac9179b494
Michael Keyes <michael.d.keyes87@gmail.com>
2020-08-15 13:38:55 -0400

    Board setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const grid = document.querySelector('.grid');
+let width = 10;
+let bombAmount = 20;
+let squares = [];
+
+// Create board
+function createBoard() {
+  // Get shuffled game array with random bombs
+  const bombsArray = Array(bombAmount).fill('bomb');
+  const emptyArray = Array(width * width - bombAmount).fill('valid');
+  const gameArray = emptyArray.concat(bombsArray);
+  const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
+  for (let i = 0; i < width * width; i++) {
+    const square = document.createElement('div');
+    square.setAttribute('id', i);
+    square.classList.add(shuffledArray[i]);
+    grid.appendChild(square);
+    squares.push(square);
+  }
+}
+
+createBoard();
